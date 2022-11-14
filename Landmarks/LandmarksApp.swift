7132e6c35ec47210276db2dd2d9075ce902d683a
Daniil Dobrodeyev <daniil.dobrodeyev@dsr-corporation.com>
2022-11-14 15:22:56 +0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Dobrodeev Daniil on 14.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
